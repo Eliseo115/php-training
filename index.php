92cc064ae8901b9a657157b93df473c03be0f46f
Eliseo Perales <e7iseo@yahoo.com>
2019-10-07 09:41:58 -0700

    start of day 10

--- index.php
+++ index.php
@@ -1,58 +1,55 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "secret";
+session_start();
+require_once("connect.php");
+require_once("functions-new.php");
+$loggedIn = $_SESSION['loggedin'];
 
-//Create connection
-$conn = new mysqli($servername,$username,$password);
+// This pulls the text from after the first / in the
+// URL and sets it to an array
+$uriSegments = explode("/", parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH));
 
-//Check connection
-if ($conn->connect_error) {
-  die("No worky " . $conn->connect_error);
-} else {
-$success = "It worked!";
+// This pulls the last item out of the URI array and sets
+// $thisPagename variabel to that item
+$thisPagename = array_pop($uriSegments);
+
+// This sets the pagename if one is provided in the url
+if ($thisPagename == "") {
+  $thisPagename = "index";
 }
-
-$thisPagename = $_GET["page"] ?? "HOME";
-
-$sql = "SELECT * FROM pagename = '$thisPagename'";
-$result = $conn->query($sql);
+// $thisPagename = $_GET["page"] ?? "HOME";
 
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title><?php //makeTitle($conn, $thisPagename); ?> </title>
 </head>
 <body>
   <nav class="nav">
     <img class="logo" src="img/hero.jpg" alt="trees">
     <div class="php-nav">
     <?php
-      $sql = "SELECT  pagename FROM test.content";
-      $result = $conn->query($sql);
-      echo "<ul>";
-      while  ( $row = $result->fetch_assoc() ) {
-        echo "<li><a href='index.php?page=" . $row['pagename'] . "'>" .$row['pagename']. "<?a></li>";     
-       }
-       echo "</ul>";
-    ?>
-    </div>
-  </nav>
+    makeNav($conn, $loggedIn);
+    echo "<p>" . $loggedIn . "</p>";
+    ?> 
+  </div>
+  
+</nav>
   <section class= "content">
     <div>
-        <?php 
-          $sql = "SELECT * FROM test.content WHERE pagename = '$thisPagename'";
-          $result = $conn->query($sql);
-          $row = $result->fetch_assoc();  
-          echo "<p>" . $row['pagetitle'] . "</p>";
-          echo "<p>" . $row['pagename'] . "</p>";
-          echo $row['pagecontent'];
-        ?>
+       <?php
+      //  echo $thisPagename;
+       makeContent($conn, $thisPagename);
+       ?>
     </div>
   </section>
   <section class="grey">
-    
+
   </section>
   
 </body>
